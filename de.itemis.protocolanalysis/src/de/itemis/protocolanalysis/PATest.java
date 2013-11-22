09a1b907b2fa236f834ce5e324174138ad7fd676
Sören Braunstein <braunstein@itemis.de>
2013-11-22 15:15:44 +0100

    Added some statechart traversing

--- de.itemis.protocolanalysis/src/de/itemis/protocolanalysis/PATest.java
+++ de.itemis.protocolanalysis/src/de/itemis/protocolanalysis/PATest.java
@@ -2,6 +2,10 @@ package de.itemis.protocolanalysis;
 
 import static org.junit.Assert.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -20,7 +24,11 @@ import org.yakindu.component.model.como.ExternalModelMapping;
 import org.yakindu.component.model.como.InterfaceMapping;
 import org.yakindu.component.model.como.Port;
 import org.yakindu.component.model.como.RefValue;
+import org.yakindu.sct.model.sgraph.Entry;
+import org.yakindu.sct.model.sgraph.Region;
 import org.yakindu.sct.model.sgraph.Statechart;
+import org.yakindu.sct.model.sgraph.Transition;
+import org.yakindu.sct.model.sgraph.Vertex;
 
 import com.google.inject.Injector;
 
@@ -73,7 +81,7 @@ public class PATest {
 				RefValue value = externalModelMapping.getValue();
 				if (value.getValue() instanceof Statechart) {
 					aStatechart = (Statechart) value.getValue();
-					// System.out.println(EmfFormatter.objToStr(statechart));
+//					System.out.println(EmfFormatter.objToStr(aStatechart));
 				}
 
 			}
@@ -97,5 +105,45 @@ public class PATest {
 		assertEquals("Couldn't resolve statechart model", "RobotArm", aStatechart.getName());
 		assertEquals("Couldn't resolve Franca model", "RobotArm", aFInterface.getName());
 	}
+	
+	@Test
+	public void traverseAllStates() {
+		Statechart statechart = getFirstStatechart();
+		Region mainRegion = statechart.getRegions().get(0);
+		assertEquals("main region", mainRegion.getName());
+		Entry entry = null;
+		for (Vertex vertex : mainRegion.getVertices()) {
+			if (vertex instanceof Entry) {
+				entry = (Entry) vertex;
+				break;
+			}
+		}
+		assertNotNull(entry);
+		Set<Vertex> trace = new HashSet<Vertex>();
+		traverse(entry.getOutgoingTransitions(), trace);
+	}
+	
+	private void traverse(EList<Transition> transitions, Set<Vertex> trace) {
+		for (Transition transition : transitions) {
+			
+			Vertex source = transition.getSource();
+			Vertex target = transition.getTarget();
+			
+			if (!trace.contains(source)) {
+				System.out.println(source.getName() + " -- [" + transition.getSpecification() + "] --> " + target.getName());
+				Set<Vertex> fork = new HashSet<>(trace);
+				fork.add(source);
+				traverse(target.getOutgoingTransitions(), trace);
+			}
+		}
+	}
 
+	private Statechart getFirstStatechart() {
+		Component component = (Component) model.getPackage().getDefinitions().get(0);
+		ExternalModelMapping componentMapping = (ExternalModelMapping) component.getComponentMapping();
+		Statechart statechart = (Statechart) componentMapping.getValue().getValue();
+		assertNotNull(statechart);
+		return statechart;
+	}
+	
 }
